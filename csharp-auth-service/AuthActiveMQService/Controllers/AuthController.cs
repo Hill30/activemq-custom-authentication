49a3682d54288dccb389d302f13a7952652cd128
npakudin <npakudin@hill30.com>
2014-12-11 19:41:47 +0530

    Added csharp service

---
+++ csharp-auth-service/AuthActiveMQService/Controllers/AuthController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AuthActiveMQService.Controllers
+{
+    public class AuthController : ApiController
+    {
+        private HttpResponseMessage Authenticate(string username, string password)
+        {
+            // replace with real validation
+            var creds = new Dictionary<string, string>
+            {
+                {"admin", "admin"},
+                {"userName", "password"},
+            };
+
+            if (username != null && creds.ContainsKey(username) && creds[username] == password)
+            {
+                return this.Request.CreateResponse(HttpStatusCode.OK, "Login OK");
+            }
+            else
+            {
+                return this.Request.CreateResponse(HttpStatusCode.Forbidden, "Wrong pair login-password");
+            }
+        }
+
+        public HttpResponseMessage Get()
+        {
+            var nvps = this.Request.GetQueryNameValuePairs();
+            var username = nvps.Where(x => x.Key == "username").Select(x => x.Value).FirstOrDefault();
+            var password = nvps.Where(x => x.Key == "password").Select(x => x.Value).FirstOrDefault();
+
+            return Authenticate(username, password);    
+        }
+
+        public HttpResponseMessage Post(Credentials creds)
+        {
+            return Authenticate(creds.Username, creds.Password);
+        }
+
+        public class Credentials
+        {
+            public string Username { get; set; }
+            public string Password { get; set; }
+        }
+    }
+}
